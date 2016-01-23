6af8456c8e2361a49af8120c42b7037c625ee363
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-23 12:56:49 -1000

    made two players display

--- public/js/states/game.js
+++ public/js/states/game.js
@@ -2,18 +2,36 @@
 //  handles sprites creation and game logic
 
 // class constructer
+(function(){
+
+  var INITIAL_POSITIONS = [
+
+    //player 1
+    { x : 100, y: 100},
+    //player 2
+    { x : 600, y :100 }
+  ];
+
 
 ToeFu.Game = function(){
 
   this.player_1;
+  this.player_2;
 
 };
 
 ToeFu.Game.prototype.create = function(){
 
   this.player_1 = new ToeFu.Player( this.game, 0);
+  this.player_2 = new ToeFu.Player( this.game, 1);
   this.game.add.existing(this.player_1);
+  this.game.add.existing(this.player_2);
 
+  //position players
+  this.player_1.x = INITIAL_POSITIONS[0].x;
+  this.player_1.y = INITIAL_POSITIONS[0].y;
+  this.player_2.x = INITIAL_POSITIONS[1].x;
+  this.player_2.y = INITIAL_POSITIONS[1].y;
 
 };
 
@@ -24,4 +42,7 @@ ToeFu.Game.prototype.update = function(){
 ToeFu.Game.prototype.shutdown = function(){
 
 };
+
+})();
+
 
